2b47e18c8b578a5ba0b16de9bf87788613b00431
jmh9072 <jmh9072@jmh9072.com>
2014-11-13 18:44:52 -0500

    Basic console user interface

--- Validation.cpp
+++ Validation.cpp
@@ -82,15 +82,55 @@ void validatePrimes(int bound, bool* goldArray, bool* checkArray)
 	std::cout << "Found all primes! \n"; 
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
-	int bound = 10000; 
-	bool * eratosArray = new bool[bound + 1];
-	bool * sundArray = new bool[bound + 1];
-
-	eratosthenesSieve(bound, eratosArray);
-	sundaramSieve(bound, sundArray);
-	validatePrimes(bound, eratosArray, sundArray); 
+	int choice = -1;
+	int bound = 0;
+	while (True)
+	{
+		cout << "Which algorithm would you like to run?" << endl;
+		cout << "0. Sieve of Eratosthenes (serial)" << endl;
+		cout << "1. Sieve of Sundaram (serial)" << endl;
+		cout << "2. Sieve of Sundaram (serial, optimized)" << endl;
+		cout << "3. Sieve of Sundaram (parallel, GPU)" << endl;
+		cout << "4. Exit" << endl;
+		cin >> choice;
+		
+		//Process exit
+		if (choice == 4)
+			return 0
+		
+		if (choice < 0 || choice > 4)
+			continue;
+			
+		cout << "What number should we find primes up to?";
+		cin >> bound;
+		
+		switch (choice)
+		{
+			case 0:
+			bool * eratosArray = new bool[bound + 1];
+			eratosthenesSieve(bound, eratosArray);
+			break;
+			
+			case 1:
+			bool * sundArray = new bool[bound + 1];
+			sundaramSieve(bound, sundArray);
+			break;
+			
+			case 2:
+			//not yet implemented
+			break;
+			
+			case 3:
+			//not yet implemented
+			break;
+			
+			default:
+			break;
+		}
+		//validatePrimes(bound, eratosArray, sundArray);
+	}
 	return 0;
 }
 
