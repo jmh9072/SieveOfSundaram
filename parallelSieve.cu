084aa65fdb2421e48da258847c175982530f4b4e
jmh9072 <jmh9072@jmh9072.com>
2014-12-01 06:34:43 -0500

    abcdefg

--- parallelSieve.cu
+++ parallelSieve.cu
@@ -70,14 +70,7 @@ __global__
 void sundPartTwoPerElementOneD(int bound, bool * findArray, bool * primeArray)
 {
 	int idx = blockDim.x * blockIdx.x + threadIdx.x; 
-	if(idx < 1)
-	{
-		return; 
-	}
-	if (idx < 2)
-	{
-		return;
-	}
+
 	if(idx == 2) //let thread 0 handle setting 2 as prime 
 	{
 		primeArray[2] = false; 
