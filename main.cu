d1ade199907e84f7c160bdc9038f19d2597a5321
jmh9072 <jmh9072@jmh9072.com>
2014-12-01 17:11:31 -0500

    no message

--- main.cu
+++ main.cu
@@ -56,9 +56,9 @@ int main()
 		
 		const dim3 a_gridSize(bound / 1024, 1, 1);
 		const dim3 a_blockSize(512, 1, 1);
-		int b_bound = (int)sqrt((double)bound) + 1; 
+		int b_bound = (int)sqrt((double)bound); 
 		const dim3 b_gridSize(b_bound / 32, b_bound/32, 1);
-		const dim3 b_blockSize(32, 32, 1);
+		const dim3 b_blockSize(1024, 1024, 1);
 		
 		const dim3 c_gridSize(bound / 16384,1,1);
 		const dim3 c_blockSize(1024, 1, 1);
