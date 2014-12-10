4974455a42a3252abc00ca05ec0916aae08002dc
jmh9072 <jmh9072@jmh9072.com>
2014-12-10 16:06:07 -0500

    b

--- sieve.cpp
+++ sieve.cpp
@@ -100,7 +100,7 @@ void sundPartTwoSerial(int bound, bool * findArray, bool * primeArray)
 ///This function compares two arrays to see if they match in the range of prime numbers
 void validatePrimes(int bound, bool* goldArray, bool* checkArray)
 {
-	for (int i = 18830; i <= 18930; i++)
+	for (int i = 0; i <= 25; i++)
 	{
 		cout << i << ".\t" << goldArray[i] << "\t" << checkArray[i] << endl;
 	}
